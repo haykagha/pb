ed854b4e31a2dc588b45a3c5c8856326dbef6b92
Hayk Aghavelyan <avaghavelyan@gmail.com>
2022-12-17 14:32:46 +0400

    first commit

---
+++ AlamofireTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireTest
+//
+//  Created by Hayk Aghavelyan on 17.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
